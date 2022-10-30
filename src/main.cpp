40d62430738b8578627f688a75d7391483f36120
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2022-10-30 01:15:12 -0500

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,25 @@ void setup() {
   pinMode(26,OUTPUT);
   //Salida D
   pinMode(27,OUTPUT);
+  /* Tercera fase del proyecto */
+  //Midiendo las corrientes en las salidas IC e ID
+  pinMode(A5,INPUT);
+  pinMode(A6,INPUT);
+  //Midiendo las tensiones en las salidas VC y VD
+  pinMode(A7,INPUT);
+  pinMode(A8,INPUT);
+  //Permitir el paso de corriente si V < 5 V e I < 1 A
+  pinMode(28,OUTPUT);
+  pinMode(29,OUTPUT);
+  // Encender led para VC y VD superior a 1 V
+  pinMode(30,OUTPUT);
+  pinMode(31,OUTPUT);
+
+  //Comunicación serial 
+  Serial.begin(9600);
+  bool medir(int valor);
 }
 
 void loop() {
-  // Código que se va a ejecutar en el microcontrolador Arduino Mega 2560
-  digitalWrite(13,LOW);
+    
 }
