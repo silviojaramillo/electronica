837ab9a0fda8f89725bd9882850049136a9c0262
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2022-11-28 07:11:54 -0500

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -214,3 +214,96 @@ void loop(){
 
     //outputsControl();
 }
+
+/* 
+
+
+#define APRETADO    0
+#define SUELTO      1
+#define APRETANDOLO 2
+#define SOLTANDOLO  3
+
+class BotonSimple {
+  private:
+    unsigned char pin;
+    unsigned char anterior, valor;
+    unsigned char estado;
+
+    unsigned long temporizador;
+    unsigned long tiempoRebote;
+  public:
+    BotonSimple(unsigned char _pin, unsigned long _tiempoRebote);
+    void actualizar();
+    int  leer();
+};
+
+BotonSimple::BotonSimple(unsigned char _pin, unsigned long _tiempoRebote=50) {
+  pin = _pin;
+  tiempoRebote = _tiempoRebote;
+  pinMode(pin, INPUT_PULLUP);
+  valor=HIGH; anterior=HIGH; estado=SUELTO;
+}
+
+void BotonSimple::actualizar() {
+  // NOTA: En el ejemplo original en vez de "pin" leia directamente el pin "2", con lo que el
+  // codigo no funciona correctamente con el pin que le hayamos asignado.
+  if ( valor==digitalRead(pin) ){
+    temporizador=0;
+  }
+  else
+  if ( temporizador==0 ) {
+    temporizador = millis();
+  }
+  else
+  if ( millis()-temporizador >= tiempoRebote ) {
+    valor = !valor;
+  }
+  if ( anterior==LOW  && valor==LOW  ) estado = APRETADO;
+  if ( anterior==LOW  && valor==HIGH ) estado = SOLTANDOLO;
+  if ( anterior==HIGH && valor==LOW  ) estado = APRETANDOLO;
+  if ( anterior==HIGH && valor==HIGH ) estado = SUELTO;
+  anterior = valor;
+}
+
+int BotonSimple::leer() { 
+  return estado;
+}
+ 
+
+BotonSimple boton(12);
+BotonSimple boton1(10);
+BotonSimple boton2(11);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+  
+void loop() {
+  boton.actualizar();
+  switch ( boton.leer() ) {
+    case SUELTO: digitalWrite(13, LOW); break;
+    case APRETANDOLO: Serial.println("Acabas de apretar el boton"); break;
+    case APRETADO: digitalWrite(13, HIGH); break;
+    case SOLTANDOLO: Serial.println("Acabas de soltar el boton"); break;
+    default: break;
+  }
+  boton1.actualizar();
+  switch ( boton1.leer() ) {
+    case SUELTO: digitalWrite(13, LOW); break;
+    case APRETANDOLO: Serial.println("Acabas de apretar el boton 1"); break;
+    case APRETADO: digitalWrite(13, HIGH); break;
+    case SOLTANDOLO: Serial.println("Acabas de soltar el boton 1"); break;
+    default: break;
+  }
+  boton2.actualizar();
+  switch ( boton2.leer() ) {
+    case SUELTO: digitalWrite(13, LOW); break;
+    case APRETANDOLO: Serial.println("Acabas de apretar el boton 2"); break;
+    case APRETADO: digitalWrite(13, HIGH); break;
+    case SOLTANDOLO: Serial.println("Acabas de soltar el boton 2"); break;
+    default: break;
+  }
+}
+
+ */
